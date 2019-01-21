b05d05985b6d01b84347616f666567a53af618c7
Brooke Woolley <brooke.woolley@gmail.com>
2019-01-21 16:27:21 +0000

    Added parameter for name

--- app.rb
+++ app.rb
@@ -22,7 +22,13 @@ get '/me' do
   "Hello me"
 end
 
-get '/cat' do
-  @cat = ["Amigo", "Oscar", " Viking"].sample
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", " Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
